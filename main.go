1cb7e2d6fe718932d7fc31dc04d4b53dc195a4b2
Petr Jahoda <jahoda@zapsi.eu>
2020-02-02 11:02:30 +0100

    new: update README.md
    Took 3 minutes

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-const version = "2020.1.2.1"
+const version = "2020.1.2.2"
 const programName = "Terminal Service"
 const deleteLogsAfter = 240 * time.Hour
 const downloadInSeconds = 10
