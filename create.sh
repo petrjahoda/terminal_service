89fd22c3fa29f64ef58ff659ea6e42a6303dddc8
Petr Jahoda <jahoda@zapsi.eu>
2020-02-29 12:54:09 +0100

    ### Changed - name of database changed to zapsi3 - proper testing for mariadb, postgres and mssql - added logging for all important methods and functions - code refactoring for better readability
    ### Fixed
    - proper closing downtimes and orders
    - proper updating data for order and downtime
    
    Took 1 minute

--- create.sh
+++ create.sh
@@ -2,6 +2,12 @@
 cd linux
 upx terminal_service_linux
 cd ..
+cd mac
+upx terminal_service_mac
+cd ..
+cd windows
+upx terminal_service_windows.exe
+cd ..
 docker rmi -f petrjahoda/terminal_service:"$1"
 docker build -t petrjahoda/terminal_service:"$1" .
 docker push petrjahoda/terminal_service:"$1"
